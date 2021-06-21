4f77f62e68a7b680235ec639d5be5fa40a6fd0d8
atlasrw <atlasrw@icloud.com>
2021-06-21 21:53:03 +0000

    Wails\Core\View

--- src/View.php
+++ src/View.php
@@ -8,6 +8,12 @@ final class View
     public static function render(string $view, array $params = [])
     {
 
+        extract(array(
+            "ASSET" => "\\Wails\\Core\\Asset",
+            "ERROR" => "\\Wails\\Core\\Error",
+            "UTILS" => "\\Wails\\Core\\Utils"
+        ));
+
         extract($params);
 
         include Asset::view($view, $params);
