c7a017419d42c4aff9a8f8a14779e64f56afe2f9
Orsat-Marylise <bwb.orsat.m@fondespierre.com>
2021-06-21 20:53:07 +0000

    Delete footer and edit color text on products page

--- views/product-list.php
+++ views/product-list.php
@@ -252,7 +252,7 @@
             <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
                 <nav id="store" class="w-full z-30 top-0 px-6 py-1">
                     <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-                        <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
+                        <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-xl "
                             href="#">
                             Store
                         </a>
@@ -343,38 +343,6 @@
         </section>
 
     </div>
-
-
-
-
-
-
-    <footer class="container mx-auto bg-white py-8 border-t border-gray-400">
-        <div class="container flex px-3 py-8 ">
-            <div class="w-full mx-auto flex flex-wrap">
-                <div class="flex w-full lg:w-1/2 ">
-                    <div class="px-3 md:px-0">
-                        <h3 class="font-bold text-gray-900">About</h3>
-                        <p class="py-4">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus
-                            commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
-                        </p>
-                    </div>
-                </div>
-                <div class="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
-                    <div class="px-3 md:px-0">
-                        <h3 class="font-bold text-gray-900">Social</h3>
-                        <ul class="list-reset items-center pt-3">
-                            <li>
-                                <a class="inline-block no-underline hover:text-black hover:underline py-1" href="#">Add
-                                    social links</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
 
 </body>
 
