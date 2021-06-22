8ea623fca5ea7797427a088cc17792af004d24e1
atlasrw <atlasrw@icloud.com>
2021-06-22 09:43:03 +0000

    models

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -20,12 +20,16 @@ final class ProductModel extends Model
     public function id() { return $this->id; }
     public function name() { return $this->name; }
     public function description() { return $this->description; }
-    public function price() { return $this->price; }
     public function stock() { return $this->stock; }
     public function picture1() { return $this->picture1; }
     public function picture2() { return $this->picture2; }
     public function picture3() { return $this->picture3; }
     public function category() { return $this->category; }
     public function VAT() { return $this->VAT; }
+    
+    public function price(bool $VAT = false)
+    {
+        return number_format(($VAT) ? $this->price + ($this->price * $this->VAT / 100) : $this->price, 2);
+    }
 
 }
