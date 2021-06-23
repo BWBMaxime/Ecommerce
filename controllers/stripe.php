add5ff06ced4aaa3bf6e29352467a53fdaaae998
Maxime <bwb.guichon.m@fondespierre.com>
2021-06-23 15:03:34 +0000

    test stripe

---
+++ controllers/stripe.php
@@ -0,0 +1,64 @@
+<?
+
+namespace Wails\Controllers;
+use Wails\Core\Controller;
+use Wails\Core\View;
+use \Stripe\Stripe;
+
+final class SessionController extends Controller
+{
+
+    /**
+     * Connexion
+     */
+    public function login()
+    {}
+
+    /**
+     * Inscription
+     */
+    public function signin()
+    {}
+
+    /**
+     * Déconnexion
+     */
+    public function logout()
+    {}
+
+};
+
+
+class SessionController extends Controller{
+    public function stripe(){
+
+    Stripe::setApiKey('sk_test_51J4moFARO0MVBs07TkoPEsX7shq0IQk6af7lQNjwH28XfF3WwfaFavsYuCevnmF31OLuLF6P8TyMK4iqQ2PUlWGS00sHMlKFX1');
+
+
+
+    $YOUR_DOMAIN = 'http://localhost:8888';
+
+    $checkout_session = \Stripe\Checkout\Session::create([
+    'payment_method_types' => ['card'],
+    'line_items' => [[
+      'price_data' => [
+        'currency' => 'usd',
+        'unit_amount' => 2000,
+        'product_data' => [
+          'name' => 'Stubborn Attachments',
+          'images' => ["https://i.imgur.com/EHyR2nP.png"],
+        ],
+      ],
+      'quantity' => 1,
+    ]],
+    'mode' => 'payment',
+    'success_url' => $YOUR_DOMAIN . '/success.html',
+    'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+
+
+    ]); json_encode(['id' => $checkout_session->id]) ;
+    View::render('checkout');
+  }
+}
+
+
