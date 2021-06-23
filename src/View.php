2b4fc6009a1ba7d76fe0c6aee4c08e62291d69d3
atlasrw <atlasrw@icloud.com>
2021-06-23 07:09:40 +0000

    View

--- src/View.php
+++ src/View.php
@@ -11,8 +11,18 @@ final class View
         extract(array(
             "_ASSET" => "\\Wails\\Core\\Asset",
             "_ERROR" => "\\Wails\\Core\\Error",
-            "_UTILS" => "\\Wails\\Core\\Utils"
+            "_UTILS" => "\\Wails\\Core\\Utils",
+            "_VIEW" => "\\Wails\\Core\\View"            
         ));
+
+        extract($params);
+
+        include Asset::view($view, $params);
+
+    }
+
+    public static function include(string $view, array $params = [])
+    {
 
         extract($params);
 
