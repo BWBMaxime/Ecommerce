4622a7d2e8d3bcc292a55c409ec00c9784cb7c99
fannymille <77175783+fannymille@users.noreply.github.com>
2021-06-25 13:49:02 +0000

    update-payment5

---
+++ views/payment.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+
+
+    <title> Payment method </title>
+
+
+
+<body>
+   
+
+	<div class="payment-method ">
+PAYMENT METHOD
+	</div>
+	
+	
+	<br>
+<p> 
+	
+	Default payment :
+<br>	
+	</div>
+	
+	<div class="border border-black-1000 border-0 border-indigo-600 bg-gray-100 flex">
+	 <img src="blackcard.jpg" width="200px" height="100px"> 
+	 
+	 
+			<div>
+				<h2 "px-2 text-gray-1000"> Nom: </h2>
+				<h2 "px-2 text-gray-1000"> Prénom: </h2>
+				<h2 "px-2 text-gray-1000"> N° of card: </h2>
+				<h2 "px-2 text-gray-1000"> Expiry date: </h2>
+			</div>
+	</div> 
+	
+	 <br> <br>
+	 
+Other payment method:	 
+	 
+		<div class="grid grid-cols-3 lg:grid-cols-2 gap-5 w-full p-4 flex">   <!-- Haut Gauche - Card -->
+			<div class="border border-black-1000 border-0 border-indigo-600 bg-gray-100 py-2 rounded-md flex">
+			<div class="flex">
+				<img src="blackcard.jpg" width="200px" height="100px">
+	 
+	 
+			<div>
+				<h2 "px-2 text-gray-1000"> Nom: </h2>
+				<h2 "px-2 text-gray-1000"> Prénom: </h2>
+				<h2 "px-2 text-gray-1000"> N° of card: </h2>
+				<h2 "px-2 text-gray-1000"> Expiry date: </h2>
+			</div>
+				
+				
+				
+				<div class="flex justify-center items-center">  <!-- PLacement de 4 cadres -->
+				 
+				
+				
+				</div>
+			  </div>
+			</div>
+			
+			
+			<div class="border border-black-1000 border-0 border-indigo-600 bg-gray-100 py-2 rounded-md flex">   <!-- Haut Droite - Paypal -->
+			  <div class="flex">
+					<img src="paypal.jpg" width="200px" height="100px">
+				
+				<div>
+				
+				
+				<h2 "text-gray-600"> Compte n°: </h2>
+				<h2 "text-gray-600"> Email: </h2>
+				</div>
+				
+				
+				
+				<div class="flex justify-center items-center">
+				 
+				</div>
+			  </div>
+			</div>
+			
+			
+			
+			
+			
+			<div class="border border-black-1000 border-0 border-indigo-600 bg-gray-100 py-2 rounded-md flex">   <!-- Bas Gauche  Paypal-->
+			  <div class="flex">
+					<img src="paypal.jpg" width="200px" height="100px">
+				<div>
+				
+			
+				<h2 "text-gray-600"> Compte:</h2>
+				<h2 "text-gray-600"> Email:</h2>
+				</div>
+				
+				
+				<div class="flex justify-center items-center">
+				
+				 
+				</div>
+			  </div>
+			</div>
+			
+			
+			
+			<div class="border border-black-1000 border-0 border-indigo-600 bg-gray-200 py-2 rounded-md flex"> <!-- Bas Droite  Card-->
+				<img src="blackcard.jpg" width="200px" height="100px">
+			  <div class="flex">
+				
+				<div>
+				
+				
+				
+				<h2 "text-gray-600"> Nom: </h2>
+				<h2 "text-gray-600"> Prénom: </h2>
+				<h2 "text-gray-600"> N° of Card: </h2>
+				<h2 "text-gray-600"> Expiry Date: </h2>
+				</div>
+				
+				<div class="flex justify-center items-center">
+				
+				
+				
+				</div>
+			  </div>
+			</div>
+		  </div>
+
+	<div>
+    <a href="#" class="bg-green-700 text-white px-5 py-3 font-bold rounded font-family: ui-serif, Georgia, Cambria, Times New Roman">
+  Valider
+</a>
+	</div>
+	
+	
+</div>	
+	
+
+<div id="add-payment-method">
+<br>
+<a href="#"> + Add new payment method </a>
+</div>
+
+
+
+
+
+</body>
+</html>
