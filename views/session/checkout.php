c03e0ede57a7f1c9845580c911a2601d58b151dc
Maxime <bwb.guichon.m@fondespierre.com>
2021-06-28 00:53:58 +0000

    'Add page checkout'

--- views/session/checkout.php
+++ views/session/checkout.php
@@ -1,2 +1,10 @@
-<p>Félicitations</p>
-<p>Commande n° <?= $checkout ?></p>
+<div class="w-full text-center rounded-lg py-24" style="background-image: linear-gradient(to right bottom, #81fbb8, #6deea6, #59e194, #43d481, #28c76f);">
+    <img class="items-center" src="<?= $ASSET::image_path('checked') ?>">
+    <div class="flex-col justify-center items-center">
+        <p class="font-bold text-3xl text-gray-100 leading-tight mt-2">Congratulations on your order ! The Order n°<?= $checkout ?></p>
+        <br>
+        <button class="rounded-lg text-green-500 font-medium bg-gray-100 p-4">Continue shopping</button>
+        
+      
+    </div>
+</div>
